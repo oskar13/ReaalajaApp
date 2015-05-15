ac9d37022919382d51317835ed6a8afcbb6d05e4
oskar13 <oskar92@tlu.ee>
2015-05-15 12:24:21 +0300

    up up up

--- topapp/core/src/com/mygdx/game/TopApp.java
+++ topapp/core/src/com/mygdx/game/TopApp.java
@@ -1,4 +1,6 @@
 package com.mygdx.game;
+import java.util.ArrayList;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -10,252 +12,222 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Plane;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class TopApp extends ApplicationAdapter implements InputProcessor {
-    SpriteBatch batch;
-    Sprite sprite;
-    Texture img;
-    World world;
-    Body body;
-    Body bodyEdgeScreen;
-    Box2DDebugRenderer debugRenderer;
-    Matrix4 debugMatrix;
-    static OrthographicCamera camera;
-    BitmapFont font;
+public class TopApp extends ApplicationAdapter implements InputProcessor {	
+	Texture texture;
+	static OrthographicCamera cam;
+	SpriteBatch batch;	
+	final Sprite[][] sprites = new Sprite[10][10];
+	//final Matrix4 matrix = new Matrix4();	
+	
+	World world;
+	private Tank juku;
+	public static ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 
-
-    float torque = 0.0f;
-    boolean drawSprite = true;
-
-    final float PIXELS_TO_METERS = 100f;
-
-    @Override
-    public void create() {
-
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
-        sprite = new Sprite(img);
-
-        sprite.setPosition(-sprite.getWidth()/2,-sprite.getHeight()/2);
-
-        world = new World(new Vector2(0, -1f),true);
-
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set((sprite.getX() + sprite.getWidth()/2) /
-                        PIXELS_TO_METERS,
-                (sprite.getY() + sprite.getHeight()/2) / PIXELS_TO_METERS);
-
-        body = world.createBody(bodyDef);
-
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(sprite.getWidth()/2 / PIXELS_TO_METERS, sprite.getHeight()
-                /2 / PIXELS_TO_METERS);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = 0.1f;
-        fixtureDef.restitution = 0.5f;
-
-        body.createFixture(fixtureDef);
-        shape.dispose();
-
-        BodyDef bodyDef2 = new BodyDef();
-        bodyDef2.type = BodyDef.BodyType.StaticBody;
-        float w = Gdx.graphics.getWidth()/PIXELS_TO_METERS;
-        // Set the height to just 50 pixels above the bottom of the screen so we can see the edge in the
-        // debug renderer
-        float h = Gdx.graphics.getHeight()/PIXELS_TO_METERS- 50/PIXELS_TO_METERS;
-        //bodyDef2.position.set(0,
-//                h-10/PIXELS_TO_METERS);
-        bodyDef2.position.set(0,0);
-        FixtureDef fixtureDef2 = new FixtureDef();
-
-        EdgeShape edgeShape = new EdgeShape();
-        edgeShape.set(-w/2,-h/2,w/2,-h/2);
-        fixtureDef2.shape = edgeShape;
-
-        bodyEdgeScreen = world.createBody(bodyDef2);
-        bodyEdgeScreen.createFixture(fixtureDef2);
-        edgeShape.dispose();
-
-        Gdx.input.setInputProcessor(this);
-
-        debugRenderer = new Box2DDebugRenderer();
-        font = new BitmapFont();
-        font.setColor(Color.BLACK);
-        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.
-                getHeight());
-    }
-
-    private float elapsed = 0;
-    @Override
-    public void render() {
-        camera.update();
-        // Step the physics simulation forward at a rate of 60hz
-        world.step(1f/60f, 6, 2);
-
-        body.applyTorque(torque,true);
-
-        sprite.setPosition((body.getPosition().x * PIXELS_TO_METERS) - sprite.
-                        getWidth()/2 ,
-                (body.getPosition().y * PIXELS_TO_METERS) -sprite.getHeight()/2 )
-        ;
-        sprite.setRotation((float)Math.toDegrees(body.getAngle()));
-
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.setProjectionMatrix(camera.combined);
-        debugMatrix = batch.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS,
-                PIXELS_TO_METERS, 0);
-        batch.begin();
-
-        if(drawSprite)
-            batch.draw(sprite, sprite.getX(), sprite.getY(),sprite.getOriginX(),
-                    sprite.getOriginY(),
-                    sprite.getWidth(),sprite.getHeight(),sprite.getScaleX(),sprite.
-                            getScaleY(),sprite.getRotation());
-
-        font.draw(batch,
-                "Restitution: " + body.getFixtureList().first().getRestitution(),
-                -Gdx.graphics.getWidth()/2,
-               Gdx.graphics.getHeight()/2 );
-        batch.end();
-        
-        
-        System.out.println(getWorldMouse());
-        
-        
-        if(Gdx.input.isKeyPressed(Input.Keys.W)){
-        	
-        	camera.translate(0, 1, 0);
-
-        }
-
-        debugRenderer.render(world, debugMatrix);
-    }
-
-    @Override
-    public void dispose() {
-        img.dispose();
-        world.dispose();
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
+	
+	@Override public void create() {
+		
+		
+		world = new World(new Vector2(0, -1f),true);
+        world.setContactListener(new ListenerClass());
+		
+		
+		texture = new Texture(Gdx.files.internal("assets/doge.png"));		
+		cam = new OrthographicCamera(10, 10 * (Gdx.graphics.getHeight() / (float)Gdx.graphics.getWidth()));			
+		cam.position.set(5, 5, 10);
+		cam.direction.set(0, 0, -1);
+		cam.near = 1;
+		cam.far = 100;		
+		//matrix.setToRotation(new Vector3(1, 0, 0), 90);
+		
+		for(int y = 0; y < 10; y++) {
+			for(int x = 0; x < 10; x++) {
+				sprites[x][y] = new Sprite(texture);
+				sprites[x][y].setPosition(x,y);
+				sprites[x][y].setSize(1, 1);
+			}
+		}
+		
+		batch = new SpriteBatch();
+		
+		Gdx.input.setInputProcessor(this);
+		
+		juku = new Tank(world, 59, 23);
+	}
+	
+	@Override public void render() {
+	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	cam.update();		
+	
+	
+    if(Gdx.input.isKeyPressed(Input.Keys.W)){
     	
-    	if(keycode == Input.Keys.W) {
-    		camera.zoom -= 0.02;
-    	}
+    	cam.translate(0, 0.1f, 0);
+
+    }
+    
+    if(Gdx.input.isKeyPressed(Input.Keys.S)){
     	
-    	if(keycode == Input.Keys.S) {
-    		camera.zoom += 0.02;
-    	}
+    	cam.rotate(0.3f);
+
+    }
+    
+
+    if(Gdx.input.isKeyPressed(Input.Keys.Q)){
     	
-    	if(keycode == Input.Keys.A) {
-    		camera.translate(-3, 0, 0);
-    	}
+    	cam.zoom += 0.1;
+
+    }
+    
+    if(Gdx.input.isKeyPressed(Input.Keys.E)){
     	
-    	if(keycode == Input.Keys.D) {
-    		camera.translate(3, 0, 0);
-    	}
+    	cam.zoom -= 0.1;
 
-
-        if(keycode == Input.Keys.RIGHT)
-            body.setLinearVelocity(1f, 0f);
-        if(keycode == Input.Keys.LEFT)
-            body.setLinearVelocity(-1f,0f);
-
-        if(keycode == Input.Keys.UP)
-            body.applyForceToCenter(0f,10f,true);
-        if(keycode == Input.Keys.DOWN)
-            body.applyForceToCenter(0f, -10f, true);
-
-        // On brackets ( [ ] ) apply torque, either clock or counterclockwise
-        if(keycode == Input.Keys.RIGHT_BRACKET)
-            torque += 0.1f;
-        if(keycode == Input.Keys.LEFT_BRACKET)
-            torque -= 0.1f;
-
-        // Remove the torque using backslash /
-        if(keycode == Input.Keys.BACKSLASH)
-            torque = 0.0f;
-
-        // If user hits spacebar, reset everything back to normal
-        if(keycode == Input.Keys.SPACE|| keycode == Input.Keys.NUM_2) {
-            body.setLinearVelocity(0f, 0f);
-            body.setAngularVelocity(0f);
-            torque = 0f;
-            sprite.setPosition(0f,0f);
-            body.setTransform(0f,0f,0f);
-        }
-
-        if(keycode == Input.Keys.COMMA) {
-            body.getFixtureList().first().setRestitution(body.getFixtureList().first().getRestitution()-0.1f);
-        }
-        if(keycode == Input.Keys.PERIOD) {
-            body.getFixtureList().first().setRestitution(body.getFixtureList().first().getRestitution()+0.1f);
-        }
-        if(keycode == Input.Keys.ESCAPE || keycode == Input.Keys.NUM_1)
-            drawSprite = !drawSprite;
-
-        return true;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-
-    // On touch we apply force from the direction of the users touch.
-    // This could result in the object "spinning"
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        body.applyForce(1f,1f,screenX,screenY,true);
-        //body.applyTorque(0.4f,true);
-        return true;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
     }
     
     
     
     
     
-    public static Vector2 getWorldMouse(){
+    if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+    	
+    	cam.direction.x -= 0.01;
+    	System.out.println("Cam direction x: " + cam.direction.x);
+    }
+    
+    if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+    	
+    	cam.direction.x += 0.01;
+    	System.out.println("Cam direction x: " + cam.direction.x);
+    }
+    
+    if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+    	
+    	cam.direction.y += 0.01;
+    	System.out.println("Cam direction y: " + cam.direction.y);
+
+    }
+    
+    if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+    	
+    	cam.direction.y -= 0.01;
+    	System.out.println("Cam direction y: " + cam.direction.y);
+
+    }
+    
+    
+    
+    
+    
+	
+			
+	batch.setProjectionMatrix(cam.combined);
+	//batch.setTransformMatrix(matrix);
+	batch.begin();
+	for(int y = 0; y < 10; y++) {
+		for(int x = 0; x < 10; x++) {
+			sprites[x][y].draw(batch);
+		}
+	}
+	
+	juku.drawTank(batch);
+	
+	batch.end();
+	
+	checkTileTouched();
+}
+	
+	
+	
+	final static Plane xyPlane = new Plane(new Vector3(0, 0, 1), 0);
+	final static Vector3 intersection = new Vector3();
+	Sprite lastSelectedTile = null;
+	
+	private void checkTileTouched() {
+		if(Gdx.input.justTouched()) {
+			Ray pickRay = cam.getPickRay(Gdx.input.getX(), Gdx.input.getY());
+			Intersector.intersectRayPlane(pickRay, xyPlane, intersection);
+			int x = (int)intersection.x;
+			int y = (int)intersection.y;
+			
+			System.out.println("Intersection: x: " +x + " y: " + y);
+			if(x >= 0 && x < 10 && y >= 0 && y < 10) {
+				if(lastSelectedTile != null) lastSelectedTile.setColor(1, 1, 1, 1);
+				Sprite sprite = sprites[x][y];
+				sprite.setColor(1, 0, 0, 1);
+				lastSelectedTile = sprite;
+			}
+		}
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+    public static  Vector2 getWorldMouse(){
     	Vector3 mousePos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+    	
+    	
+    	Ray pickRay = cam.getPickRay(Gdx.input.getX(), Gdx.input.getY());
+		Intersector.intersectRayPlane(pickRay, xyPlane, intersection);
+		float x = intersection.x;
+		float y = intersection.y;
         
-        
-        return new Vector2(camera.unproject(mousePos).x, camera.unproject(mousePos).y);
+        return new Vector2(x, y);
     	
     }
+	
+	
 }
